930e822342f6ec2fcee224d59071db06033aee20
Anuken <arnukren@gmail.com>
2017-06-26 15:17:12 -0400

    Changed backend to lwjgl3

--- desktop/src/space/homecore/neospinner/desktop/DesktopLauncher.java
+++ desktop/src/space/homecore/neospinner/desktop/DesktopLauncher.java
@@ -1,12 +1,16 @@
 package space.homecore.neospinner.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
 import space.homecore.neospinner.NeoSpinner;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new NeoSpinner(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		//this disables vsync, so you get unlimited FPS
+		//comment out to enable vsync
+		config.useVsync(false);
+		new Lwjgl3Application(new NeoSpinner(), config);
 	}
 }
