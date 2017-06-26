82dff73de0e71722db64430c8fa122d12001facd
Epowerj <epowerj@gmail.com>
2017-06-25 23:39:01 -0700

    Added basic project

---
+++ desktop/src/space/homecore/neospinner/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package space.homecore.neospinner.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import space.homecore.neospinner.NeoSpinner;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NeoSpinner(), config);
+	}
+}
---
+++ html/src/space/homecore/neospinner/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package space.homecore.neospinner.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import space.homecore.neospinner.NeoSpinner;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new NeoSpinner();
+        }
+}
